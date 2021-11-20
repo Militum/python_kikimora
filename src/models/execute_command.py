9a5e5a976ad72d43f98c314d316fd0cc65cbadca
Militum <wind.of.second@gmail.com>
2021-11-21 00:11:55 +0900

    helpコマンド実装

--- src/models/execute_command.py
+++ src/models/execute_command.py
@@ -1,7 +1,15 @@
+from commands import help_command
+
+# 共通バリデーション
+def validation(options: dict)->bool:
+    if 'name' not in options:
+        return False
+
+    return True
+
 def execute(options:dict)->dict:
-    return {
-        "type": 4, # InteractionResponseType.ChannelMessageWithSource
-        "data": {
-            "content": 'sample text'
-        }
-    }
+    if validation(options) == False:
+        raise Exception("不正アクセス")
+
+    # どれにも当てはまらない場合はhelpコマンドと見做す
+    return help_command.execute(options=options)
