fd06f29213e3724167abfbc8d0e8af5e5a350079
Militum <wind.of.second@gmail.com>
2021-11-21 13:59:46 +0900

    discordから入力されたコマンドをパースして実行するまでの仕組みのベースを作成

--- src/commands/__init__.py
+++ src/commands/__init__.py
@@ -1,1 +1,11 @@
-from .help_command import execute
+__all__ = [
+    'CommandBase',
+    'InitializeCommand',
+    'HelpCommand'
+]
+
+from .command_base import CommandBase
+from .initialize_command import InitializeCommand
+from .help_command import HelpCommand
+
+from .command_factory import create
--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,2 +1,4 @@
 from .execute_command import execute
 from .register_command import execute
+
+from .initialize import execute
