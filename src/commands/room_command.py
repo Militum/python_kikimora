7e63fcd3b4b8fe171fcfe7879a0f7d6b5f16d58f
Militum <wind.of.second@gmail.com>
2021-11-23 00:55:14 +0900

    セッションルーム作成コマンド実装

--- src/commands/room_command.py
+++ src/commands/room_command.py
@@ -1,6 +1,7 @@
 
 import constants
 from commands import CommandBase
+from discord import create_channnel
 
 class RoomCommand(CommandBase):
 
@@ -8,17 +9,44 @@ class RoomCommand(CommandBase):
         super().__init__()
 
     def validate(self, member: dict, options: dict) -> None:
+        if 'user' not in member:
+            raise Exception('パラメータ不足')
+
+        if 'username' not in member.get('user'):
+            raise Exception('パラメータ不足')
+
         if 'channel_name' not in options:
+            raise Exception('パラメータ不足')
+
+        if 'is_oneoff' not in options:
             raise Exception('パラメータ不足')
 
         super().validate(member, options)
 
     def execute(self, member: dict, options: dict)->dict:
-        text = 'RoomCommand'
+        channel_name = options.get('channel_name')
+        is_oneoff_session = options.get('is_oneoff')
+        is_voice_session = options.get('is_voice_session', True)
 
+        # 入力値を元にチャンネルを作成
+        response = create_channnel.execute(
+            channel_name,
+            constants.SESSION_TEXT_CATEGORY_ID if is_oneoff_session == False else constants.CAMPAIGN_TEXT_CATEGORY_ID,
+            constants.GUILD_CHANNEL_TYPES["GUILD_TEXT"]
+        )
+
+        # テキストセッションであればVCチャンネルはいらないので最初から作らない
+        if is_voice_session == True:
+            create_channnel.execute(
+                channel_name,
+                constants.SESSION_VC_CATEGORY_ID if is_oneoff_session == False else constants.CAMPAIGN_VC_CATEGORY_ID,
+                constants.GUILD_CHANNEL_TYPES["GUILD_VOICE"]
+            )
+
+        user_name = member.get('user').get('username')
         return {
             "type": constants.RESPONSE_TYPES['MESSAGE_WITH_SOURCE'],
             "data": {
-                "content": text
+                "content": "{}が<#{}>のセッションルームを作りました。良い卓を！".format(user_name, response.get('id'))
             }
         }
