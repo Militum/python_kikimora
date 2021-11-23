c89b0c49888b15e9519cea1843e16fb710de40fc
Militum <wind.of.second@gmail.com>
2021-11-23 16:17:58 +0900

    チャンネル削除コマンド実装 (#15)

--- src/commands/delete_command.py
+++ src/commands/delete_command.py
@@ -1,6 +1,7 @@
 
 import constants
 from commands import CommandBase
+from discord import delete_channel, get_channel
 
 class DeleteCommand(CommandBase):
 
@@ -11,11 +12,28 @@ class DeleteCommand(CommandBase):
         super().validate(member, options)
 
     def execute(self, member: dict, options: dict, channel_id: str)->dict:
-        text = 'DeleteCommand'
+        # コマンドを実行したチャンネルは必ずある
+        text_channel = get_channel.get_channel_by_id(channel_id)
+
+        # テキストチャンネルと同じ名前のVCチャンネルを取得して連動して削除する
+        target_channel_name = text_channel.get('name')
+        voice_channels = get_channel.get_channels_by_name(
+            target_channel_name,
+            constants.GUILD_CHANNEL_TYPES["GUILD_VOICE"]
+        )
+        for voice_channel in voice_channels:
+            delete_channel.execute(
+                channel_id=voice_channel.get('id')
+            )
+
+        delete_channel.execute(channel_id=channel_id)
 
         return {
-            "type": constants.RESPONSE_TYPES['MESSAGE_WITH_SOURCE'],
+            "type": constants.RESPONSE_TYPES['MESSAGE_NO_SOURCE'],
             "data": {
-                "content": text
+                "tts": False,
+                "content": 'Delete Channel.',
+                "embeds": [],
+                "allowed_mentions": []
             }
         }
