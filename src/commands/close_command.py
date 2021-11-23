cd9710e1c4c6b7454e344408d2bbaa9e490091e0
Militum <wind.of.second@gmail.com>
2021-11-23 15:47:00 +0900

    締め切りコマンド実装

--- src/commands/close_command.py
+++ src/commands/close_command.py
@@ -1,6 +1,7 @@
 
 import constants
 from commands import CommandBase
+from discord import rename_channel, get_channel
 
 class CloseCommand(CommandBase):
 
@@ -11,11 +12,25 @@ class CloseCommand(CommandBase):
         super().validate(member, options)
 
     def execute(self, member: dict, options: dict, channel_id: str)->dict:
-        text = 'CloseCommand'
+        # コマンドを実行したチャンネルは必ずある
+        text_channel = get_channel.get_channel_by_id(channel_id)
 
+        target_channel_name = text_channel.get('name')
+
+        if target_channel_name.startswith('〆'):
+            return {
+                "type": constants.RESPONSE_TYPES['MESSAGE_WITH_SOURCE'],
+                "data": {
+                    "content": "既に募集を締め切っています"
+                }
+            }
+
+        rename_channel.execute(channel_id=channel_id, new_channel_name='〆{}'.format(target_channel_name))
+
+        user_name = member.get('user').get('username')
         return {
             "type": constants.RESPONSE_TYPES['MESSAGE_WITH_SOURCE'],
             "data": {
-                "content": text
+                "content": "{}が募集を締め切りました".format(user_name)
             }
         }
